e254bf1887df7a7a5df5c85616313450c6abe144
Xiang Xiao <xxiao23@gmail.com>
2022-12-30 16:52:35 -0800

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Xiang Xiao on 12/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
