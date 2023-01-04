3929dde42cdb9d445ac3de37e88d294fea4d03ad
Xiang Xiao <xxiao23@gmail.com>
2023-01-03 21:37:45 -0800

    [chapter 7] i18n and L10n

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -26,8 +26,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
   }
   
   @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-    if let text = textField.text, let value = Double(text) {
-      fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+    if let text = textField.text, let number = numberFormatter.number(from: text) {
+      fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
     } else {
       fahrenheitValue = nil
     }
@@ -63,8 +63,12 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
   func textField(_ textField: UITextField,
                  shouldChangeCharactersIn range: NSRange,
                  replacementString string: String) -> Bool {
-    let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
-    let replacementTextHasDecimalSeparator = string.range(of: ".")
+    let currentLocale = Locale.current
+    let decimalSeparator = currentLocale.decimalSeparator ?? "."
+    
+    let existingTextHasDecimalSeparator
+      = textField.text?.range(of: decimalSeparator)
+    let replacementTextHasDecimalSeparator = string.range(of: decimalSeparator)
     
     if existingTextHasDecimalSeparator != nil,
        replacementTextHasDecimalSeparator != nil {
