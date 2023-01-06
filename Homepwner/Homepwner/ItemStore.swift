9d4109f1555d0198a65de2ab5d47b04b88917eba
Xiang Xiao <xxiao23@gmail.com>
2023-01-05 21:29:22 -0800

    [chapter11] editing UITableView

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -19,9 +19,24 @@ class ItemStore {
     return newItem
   }
 
-  init() {
-    for _ in 0..<5 {
-      createItem()
+  func removeItem(_ item: Item) {
+    if let index = allItems.firstIndex(of: item) {
+      allItems.remove(at: index)
     }
+  }
+  
+  func moveItem(from fromIndex: Int, to toIndex: Int) {
+    if fromIndex == toIndex {
+      return
+    }
+    
+    // Get reference to object being moved so you can reinsert it
+    let moveItem = allItems[fromIndex]
+    
+    // Remove item from array
+    allItems.remove(at: fromIndex)
+    
+    // Insert item in array at new location
+    allItems.insert(moveItem, at: toIndex)
   }
 }
