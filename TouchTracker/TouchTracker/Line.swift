81ace34b43919715233cae232d57387ce69798f9
Xiang Xiao <xxiao23@gmail.com>
2023-01-28 13:19:56 -0800

    [chapter 18] touch events and UIResponder

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Xiang Xiao on 1/28/23.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+  var begin = CGPoint.zero
+  var end = CGPoint.zero
+}
